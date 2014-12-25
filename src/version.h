cc29a0846a5cd52c69d194fd3445368b1de52e19
Minato <bitcreditscc@gmail.com>
2014-12-26 00:47:01 +0200

    another try

--- src/version.h
+++ src/version.h
@@ -31,10 +31,10 @@ extern const std::string CLIENT_DATE;
 static const int PROTOCOL_VERSION = 70003;
 
 // initial proto version, to be increased after version/verack negotiation
-static const int INIT_PROTO_VERSION = 209;
+static const int INIT_PROTO_VERSION = 211;
 
 // disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 211;
+static const int MIN_PEER_PROTO_VERSION = 212;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
